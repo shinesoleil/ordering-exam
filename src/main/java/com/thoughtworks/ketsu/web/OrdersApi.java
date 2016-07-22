db30393961ef59bb06bb1cb5f38085608aa92f13
Hao Pan <hao_pan@outlook.com>
2016-07-22 13:54:01 +0800

    return 201 when post order with params

--- src/main/java/com/thoughtworks/ketsu/web/OrdersApi.java
+++ src/main/java/com/thoughtworks/ketsu/web/OrdersApi.java
@@ -1,11 +1,15 @@
 package com.thoughtworks.ketsu.web;
 
+import com.thoughtworks.ketsu.domain.order.Order;
+import com.thoughtworks.ketsu.domain.user.User;
 import com.thoughtworks.ketsu.domain.user.UserRepository;
 
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.HashMap;
+import java.util.Optional;
 
 @Path("users/{userId}/orders")
 public class OrdersApi {
@@ -14,7 +18,17 @@ public class OrdersApi {
   UserRepository userRepository;
 
   @POST
-  public Response createOrder() {
-    return Response.status(201).build();
+  @Consumes(MediaType.APPLICATION_JSON)
+  public Response createOrder(HashMap<String, Object> info,
+                              @PathParam("userId") int userId) {
+    User user = userRepository.findById(userId).get();
+
+    Optional<Order> orderOptional = user.placeOrder(info);
+
+    if (orderOptional.isPresent()) {
+      return Response.status(201).build();
+    } else {
+      throw new WebApplicationException(Response.Status.BAD_REQUEST);
+    }
   }
 }
