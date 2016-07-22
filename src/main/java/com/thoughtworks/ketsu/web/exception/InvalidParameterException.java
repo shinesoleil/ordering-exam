f32594ecff3962b4617ac4b36725e60079ddae59
Hao Pan <hao_pan@outlook.com>
2016-07-22 11:14:57 +0800

    return 400 when post product with invalid parameters

--- src/main/java/com/thoughtworks/ketsu/web/exception/InvalidParameterException.java
+++ src/main/java/com/thoughtworks/ketsu/web/exception/InvalidParameterException.java
@@ -1,6 +1,11 @@
 package com.thoughtworks.ketsu.web.exception;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InvalidParameterException extends RuntimeException {
+    private List<InvalidExceptionMessage> invalidExceptionMessageList;
+
     public InvalidParameterException(String message) {
         super(message);
     }
@@ -11,5 +16,17 @@ public class InvalidParameterException extends RuntimeException {
 
     public InvalidParameterException(Exception e) {
         super(e);
+    }
+
+    public InvalidParameterException(List<String> invalidParamList) {
+        invalidExceptionMessageList = new ArrayList<>();
+
+        for (String invalidParam: invalidParamList) {
+            invalidExceptionMessageList.add(new InvalidExceptionMessage(invalidParam));
+        }
+    }
+
+    public List<InvalidExceptionMessage> getInvalidExceptionMessageList() {
+        return invalidExceptionMessageList;
     }
 }
